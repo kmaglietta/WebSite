583378b9d72c1dcf6dd316e5baf4974415e9d4e4
Kyle Maglietta <Kyle Maglietta>
2017-01-03 14:28:28 -0500

    Added firebase, angularfire, and js to conect t ofirebase console

---
+++ js/index.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+ var config = {
+   apiKey: "AIzaSyCFSB076c-Vk9Yk6nJtDa3xEXPrnxQ-ZXQ",
+   authDomain: "website-e3779.firebaseapp.com",
+   databaseURL: "https://website-e3779.firebaseio.com",
+   storageBucket: "website-e3779.appspot.com",
+   messagingSenderId: "10576339842"
+ };
+ firebase.initializeApp(config);
