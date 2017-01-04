16ccfdc9b50ef015142188cd08a8a6064cf6d135
Kyle Maglietta <Kyle Maglietta>
2017-01-03 19:09:04 -0500

    Implemented messages angular code

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
-// Initialize Firebase
- var config = {
-   apiKey: "AIzaSyCFSB076c-Vk9Yk6nJtDa3xEXPrnxQ-ZXQ",
-   authDomain: "website-e3779.firebaseapp.com",
-   databaseURL: "https://website-e3779.firebaseio.com",
-   storageBucket: "website-e3779.appspot.com",
-   messagingSenderId: "10576339842"
- };
- firebase.initializeApp(config);
+var homePageApp = angular.module('homePageApp', []);
+
+homePageApp.controller('HomePageController', function
+HomePageController($scope){
+  $scope.messages = [
+    {
+      date: 'January 5, 2017',
+      body: 'this is a test'
+    },
+    {
+      date: 'December 30 2016',
+      body: 'this is a nother test'
+    }
+  ];
+});
