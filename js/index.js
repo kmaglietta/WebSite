244cfa83eb571da9ead2c0c01a7c52a67f07025a
Kyle Maglietta <Kyle Maglietta>
2017-01-09 14:57:33 -0500

    Added a personal projects page and finished adding all past project links

--- js/index.js
+++ js/index.js
@@ -10,6 +10,9 @@ homePageApp.config(function($routeProvider){
   })
   .when("/messages", {
     templateUrl : "messages.html"
+  })
+  .when("/personal", {
+    templateUrl : "personal.html"
   });
 
 });
@@ -24,6 +27,14 @@ HomePageController($scope){
     {
       date: 'December 30 2016',
       body: 'this is a nother test'
+    }
+  ];
+
+  $scope.personal = [
+    {
+      link: '#',
+      title: 'Car Maintnece App',
+      body: 'A web app that helps drivers keep track of car maintnece and fule economy'
     }
   ];
 
@@ -47,6 +58,46 @@ HomePageController($scope){
       link: 'http://lamp.cse.fau.edu/~kmaglietta2013/hw4/index.html',
       title: 'To-Do List',
       body: 'A to-do list using jQuery'
+    },
+    {
+      link: 'http://lamp.cse.fau.edu/~kmaglietta2013/hw5/index.php',
+      title: 'Currency Converter',
+      body: 'Introduction into PHP'
+    },
+    {
+      link: 'http://lamp.cse.fau.edu/~kmaglietta2013/hw6/index.php',
+      title: 'Baby names',
+      body: 'Understanding the relationship between PHP and MYSQL'
+    },
+    {
+      link: 'http://lamp.cse.fau.edu/~kmaglietta2013/hw7/index.php',
+      title: 'The Wall (Start)',
+      body: 'A glips int web security and validation'
+    },
+    {
+      link: 'http://lamp.cse.fau.edu/~kmaglietta2013/hw8/index.php',
+      title: 'The Wall (End)',
+      body: 'Handle image upload and storage'
+    },
+    {
+      link: 'https://bookmark-app-ed51e.firebaseapp.com/',
+      title: 'Firebase',
+      body: 'A glipse into the power of firebase'
+    },
+    {
+      link: 'http://lamp.cse.fau.edu/~kmaglietta2013/project3/index.html',
+      title: 'Angular',
+      body: 'Learing how to use angualr along with local storage'
+    },
+    {
+      link: 'https://again-kmaglietta.herokuapp.com/#/',
+      title: 'Heroku',
+      body: 'Learing about Heroku and what it can offer'
+    },
+    {
+      link: 'https://score-22dc8.firebaseapp.com/',
+      title: 'Firebase (Main)',
+      body: 'Creating a usful app using the firebase AIP'
     }
   ];
 
